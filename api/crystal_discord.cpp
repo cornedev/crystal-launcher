e2aa46b2251729b415423ba2ddfa906eb805376e
cornedev <corneproductions@gmail.com>
2026-05-04 09:08:33 +0200

    Add new functions for discord-rich-presence.
    Add:
    - StartRichPresence() for starting and initializing discord-rich-presence.
    - SetRichPresenceSmall() for setting the small image with a minecraft head.
    - RemoveRichPresenceSmall() for removing the small image.
    - SetRichPresenceDetails() to change the rich-presence details.

---
+++ api/crystal_discord.cpp
@@ -0,0 +1,69 @@
+#include "api.hpp"
+
+namespace crystal
+{
+
+// - helper defines.
+static DiscordRichPresence presence;
+static std::string smallimagekey;
+static std::string smallimagetext;
+static std::string rpcdetails;
+static std::string buttonlabel;
+static std::string buttonurl;
+// - end helper defines.
+
+namespace discord
+{
+    bool StartRichPresence()
+    {
+        DiscordEventHandlers handlers{};
+        std::string appid = "1500192183431467058";
+        Discord_Initialize(appid.c_str(), &handlers, 1, nullptr);
+
+        std::memset(&presence, 0, sizeof(presence));
+
+        presence.largeImageKey = "crystal";
+        presence.largeImageText = "crystal launcher";
+
+        buttonlabel = "view project";
+        buttonurl = "https://github.com/cornedev/crystal-launcher";
+        presence.button1_label = buttonlabel.c_str();
+        presence.button1_url = buttonurl.c_str();
+        
+        SetRichPresenceDetails("idling...");
+
+        Discord_UpdatePresence(&presence);
+        return true;
+    }
+
+    bool SetRichPresenceSmall(const std::string& username, const std::string& uuid)
+    {
+        smallimagekey = "https://api.mcheads.org/head/" + uuid + "/32";
+        smallimagetext = username;
+        presence.smallImageKey = smallimagekey.c_str();
+        presence.smallImageText = smallimagetext.c_str();
+
+        Discord_UpdatePresence(&presence);
+        return true;
+    }
+
+    bool RemoveRichPresenceSmall()
+    {
+        presence.smallImageKey = nullptr;
+        presence.smallImageText = nullptr;
+
+        Discord_UpdatePresence(&presence);
+        return true;
+    }
+
+    bool SetRichPresenceDetails(const std::string& details)
+    {
+        rpcdetails = details;
+        presence.details = rpcdetails.c_str();
+
+        Discord_UpdatePresence(&presence);
+        return true;
+    }
+}
+
+}
